e10d978e61b1297c20486e2db0e60be903953a57
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-01-24 08:46:19 -0200

    Aula 9 - Organizando nossas queries

--- financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -12,30 +12,26 @@ import br.com.caelum.financas.util.JPAUtil;
 public class TesteFuncoesJPQL {
 
 	public static void main(String[] args) {
+
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
 		Conta conta = new Conta();
 		conta.setId(3);
 
-		String jpql = "select distinct avg(m.valor) from Movimentacao m where m.conta = :pConta "
-				+ "and m.tipoMovimentacao = :pTipo " + "group by m.data";
+		TypedQuery<Double> typedQuery = em.createNamedQuery("MediasPorDiaETipo", Double.class);
+		typedQuery.setParameter("pConta", conta);
+		typedQuery.setParameter("pTipo", TipoMovimentacao.SAIDA);
 
-		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
+		List<Double> medias = typedQuery.getResultList();
 
-		List<Double> medias = (List<Double>) query.getResultList();
-
-		/*System.out.println("A média do dia 26 é: " + medias.get(0));
-		System.out.println("A média do dia 27 é: " + medias.get(1));*/
-		
 		for (Double media : medias) {
-		    System.out.println("A média é: " + media);
-		}		
+			System.out.println("A média é: " + media);
+		}
 
 		em.getTransaction().commit();
 		em.close();
+
 	}
 
 }
